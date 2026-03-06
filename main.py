37a13e67d5a651ab7dec986c7d51f75f0f43d532
qztweII <terencexfwu@gmail.com>
2026-03-06 16:44:03 +1100

    chore: Did part of pseudocode for main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+#Import all of the various script files
+
+#using = True
+#WHILE using
+#   type = Ask user for actions
+#   IF type in ["Fruit Lookup", "Nutrition"]:
+#       search = Ask user for search term
+#       IF search == "Back":
+#           continue
+#       ENDIF
+#       request = fruit_lookup(search)
+#       IF request not has error:
+#           Save search history
+#           Print all of the data line by line
+#       ENDIF
+#       ELSE:
+#           Show an error message
+#       ENDELSE
+#   ENDIF
+#   ELSE IF type == "Exit":
+#       using = False
+#       
+#ENDWHILE
