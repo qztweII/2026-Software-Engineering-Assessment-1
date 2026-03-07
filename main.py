84fbf18168467c5d34a81e2564c4a9bf12df3ae1
qztweII <terencexfwu@gmail.com>
2026-03-07 15:01:53 +1100

    chore: Completed pseudocode for main.py

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 #Import all of the various script files
-
+#BEGIN main.py
 #using = True
+#compare_list = []
+#wanna_compare = False #This is for the compare feature that is wired back to the search function. 
 #WHILE using
-#   type = Ask user for actions
-#   IF type in ["Fruit Lookup", "Nutrition"]:
+#   IF not wanna_compare:
+#       type = Ask user for actions
+#   ENDIF
+#   IF type in ["Fruit Lookup", "Nutrition"] or wanna_compare:
 #       search = Ask user for search term
 #       IF search == "Back":
 #           continue
 #       ENDIF
-#       request = fruit_lookup(search)
+#       request = fruitLookup(search)
 #       IF request not has error:
-#           Save search history
+#           saveSearchHistory
 #           Print all of the data line by line
+#
+#           IF wanna_compare:
+#               finished_comparing = Ask user if finished comparing
+#               IF finished_comparing:
+#                   compareFruits
+#           ENDIF
+#           ELSE:
+#               comparing = Ask user if want to compare
+#               IF comparing == "yes":
+#                   wanna_compare = True
+#               ENDIF
+#           ENDELSE
+#           
 #       ENDIF
 #       ELSE:
 #           Show an error message
 #       ENDELSE
 #   ENDIF
-#   ELSE IF type == "Exit":
+#   ELIF type == "Exit":
 #       using = False
-#       
+#   ENDELIF
 #ENDWHILE
+#END main.py
