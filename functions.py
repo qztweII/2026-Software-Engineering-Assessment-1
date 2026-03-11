4ed9c57d5b4929935044e4814eac0c9633657622
qztweII <terencexfwu@gmail.com>
2026-03-11 16:58:44 +1100

    feat: Search History created
    Note that deleting search history after a certain amount of entries is not implemented.

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import requests
-import matplotlib
+#import matplotlib
+import json
 
 APILink = "https://fruityvice.com/api/fruit/"
 
@@ -18,6 +19,25 @@ def choices(list, query):
             break
     return list[choice]
         
+def saveSearchHistory(thing):
+    with open("history.json", "r") as f:
+        lines = f.read()
+        if len(lines) == 0:
+            first_thing = True
+        else:
+            first_thing = False
+
+    lines = lines[1:-1]
+    lines = (f"[{lines}{'' if first_thing else ','}{json.dumps(thing)}]")
+    
+    with open("history.json", "w") as f:
+        f.write(lines)
+
+def readSearchHistory():
+    with open("history.json", "r") as f:
+        history = json.loads(f)
+    
+    return history
 
 def fruitLookup(want):
     global APILink
@@ -25,6 +45,8 @@ def fruitLookup(want):
         fruit = input("Search fruits: ")
         fruit_data = requests.get(f"{APILink}{fruit}")
         fruit_data = fruit_data.json()
+        if not "error" in fruit_data:
+            fruit_data = [fruit_data]
     elif want == "Nutrition": 
         nutritions = ["calories", "fat", "sugar", "carbohydrates", "protein"]
         nutrition = choices(nutritions, "Choose nutrient: ")
@@ -45,22 +67,26 @@ def fruitLookup(want):
         test = f"{APILink}{nutrition}?min={min}&max={max}"
         fruit_data = requests.get(test)
         fruit_data = fruit_data.json()
-        print(fruit_data)
-        if not type(fruit_data) == "dict": #Errors come as a dict
-            if len(fruit_data) > 1:
-                names = []
-                for i in fruit_data:
-                    names.append(i["name"])
-                fruit_chosen = choices(names, "Which fruit do you want to choose?")
-                for i in range(len(fruit_data)): #Get rid of everything that is not the fruit I want
-                    if fruit_data[i]["name"] != fruit_chosen:
-                        fruit_data[i] = "None"
-                fruit_data = [n for n in fruit_data if n != "None"] #Actually gets rid of it
-                fruit_data = fruit_data[0] #Turn fruit_data into dictionary from dictionary in list
+    if not type(fruit_data) == "dict": #Errors come as dictionaries
+        if len(fruit_data) > 1: #If a list of fruits come in
+            names = []
+            for i in fruit_data:
+                names.append(i["name"])
+            fruit_chosen = choices(names, "Which fruit do you want to choose?")
+            for i in range(len(fruit_data)): #Get rid of everything that is not the fruit I want
+                if fruit_data[i]["name"] != fruit_chosen:
+                    fruit_data[i] = "None"
+            fruit_data = [n for n in fruit_data if n != "None"] #Actually gets rid of it
+        fruit_data = fruit_data[0] #Turn fruit_data into dictionary from dictionary in list
+        if want == "Nutrition":
+            saveSearchHistory({"type":"Nutrition", "nutrition":nutrition, "max": max, "min": min})
+        else:
+            saveSearchHistory({"type":"Name", "name":fruit})
+
+
     return fruit_data
 
-def saveSearchHistory(thing):
-    pass
+
 
 
 def displayData(thing):
